d331c0b73e62ebe2ad8e8777f144d12d5325b005
Alex Jones <alexander.t.jones@gmail.com>
2012-08-17 07:57:49 -0800

    Get this into the hub
    Test.html file which loads the domexchange.js file.
    domexchange.js which uses Dojo 1.8 to help exchange elements.

---
+++ domexchange.js
@@ -0,0 +1,18 @@
+define([
+    "dojo/dom", "dojo/query", "dojo/dom-construct", "dojo/NodeList"
+], function (dom, query, domConstruct, nodeList) {
+    return {
+        exchange:function () {
+            var nodes = query("body > *");
+            nodes.forEach(function (node, index, nodeList) {
+                console.log(nodeList);
+                var idx = Math.floor(Math.random() * nodes.length)
+
+                domConstruct.place(node, nodeList[idx], "after");
+            });
+
+
+        }
+
+    };
+});
